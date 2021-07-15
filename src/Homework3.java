38658ceace114cded60089656e747e6c085188c8
Mihail Nikitin <nitinin.miha@icloud.com>
2021-07-15 20:36:14 +0300

    Made 1'st task.

---
+++ src/Homework3.java
@@ -0,0 +1,9 @@
+public class Homework3 {
+    public static void main(String[] args) {
+        //Task1
+        int[] a = {1,0,1,1,0,0,1,0,1,0,1,1,1,1};
+        for (int i = 0;i<a.length;++i)
+            a[i] = 1-a[i]; //Красивый аналог условию
+
+    }
+}
